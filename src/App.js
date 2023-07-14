d1e79ebb730452be9abd1075b07a9209dba6eda5
Success Aje <successaje7@gmail.com>
2023-07-14 18:32:18 +0100

    simple html codds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Greet from "./components/Greet";
+import { Component } from 'react';
+
+import Welcome from "./components/Welcome"
+
+// class Apps extends Component {
+//   render() {
+//     return (
+
+//     );
+//   };
+// };
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Greet />
+     <Welcome />
     </div>
   );
 }
